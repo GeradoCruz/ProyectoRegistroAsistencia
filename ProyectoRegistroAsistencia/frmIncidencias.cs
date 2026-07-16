28cb86837feb726d250aeb5730e57932d54803cc
Gerardo Cruz <gerardhdez123@gmail.com>
2026-07-16 05:13:11 -0600

    cambios

--- ProyectoRegistroAsistencia/frmIncidencias.cs
+++ ProyectoRegistroAsistencia/frmIncidencias.cs
@@ -15,33 +15,9 @@ namespace ProyectoRegistroAsistencia
         public frmIncidencias()
         {
             InitializeComponent();
-            btnJustificar.Click += btnJustificar_Click;
-        }
 
-        private void RefrescarGrid()
-        {
-            // TODO: volver a cargar dgvIncidencias desde la base de datos
-        }
-
-        private void btnJustificar_Click(object? sender, EventArgs e)
-        {
-            if (dgvIncidencias.CurrentRow == null)
-            {
-                MessageBox.Show("Selecciona una incidencia de la lista para justificar.", "Staff Asistence",
-                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
             }
 
-            using (var frm = new frmJustificante())
-            {
-                // TODO: precargar los datos de la incidencia seleccionada
-                // frm.CargarDatos(nombre, departamento, incidencia, fecha);
 
-                if (frm.ShowDialog(this) == DialogResult.OK)
-                {
-                    RefrescarGrid();
-                }
-            }
-        }
     }
 }
--- ProyectoRegistroAsistencia/frmPuestos.cs
+++ ProyectoRegistroAsistencia/frmPuestos.cs
@@ -15,43 +15,7 @@ namespace ProyectoRegistroAsistencia
         public frmPuestos()
         {
             InitializeComponent();
-            btnNuevo.Click += btnNuevo_Click;
-            btnEditar.Click += btnEditar_Click;
-        }
 
-        private void RefrescarGrid()
-        {
-            // TODO: volver a cargar dgvPuestos desde la base de datos
-        }
-
-        private void btnNuevo_Click(object? sender, EventArgs e)
-        {
-            using (var frm = new frmNuevoPuesto())
-            {
-                if (frm.ShowDialog(this) == DialogResult.OK)
-                {
-                    RefrescarGrid();
-                }
-            }
-        }
-
-        private void btnEditar_Click(object? sender, EventArgs e)
-        {
-            if (dgvPuestos.CurrentRow == null)
-            {
-                MessageBox.Show("Selecciona un puesto de la lista para editar.", "Staff Asistence",
-                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-
-            // TODO: precargar los datos del puesto seleccionado en frmNuevoPuesto
-            using (var frm = new frmNuevoPuesto())
-            {
-                if (frm.ShowDialog(this) == DialogResult.OK)
-                {
-                    RefrescarGrid();
-                }
-            }
         }
     }
 }
