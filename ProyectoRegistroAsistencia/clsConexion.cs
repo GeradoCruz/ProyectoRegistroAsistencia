bd66d9f8cc14b2e5b8d620cadd3f4a1e00c62bd6
Gerardo Cruz <gerardhdez123@gmail.com>
2026-07-19 21:35:30 -0600

    implementacion de camara

--- ProyectoRegistroAsistencia/clsConexion.cs
+++ ProyectoRegistroAsistencia/clsConexion.cs
@@ -1,35 +1,52 @@
 ﻿using MySqlConnector;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProyectoRegistroAsistencia
 {
     internal class clsConexion
     {
         private string host = "189.240.192.140";
-        private string bd = "equipo1_staff_asistence";
-        private string usuario = "usuario1GB";
-        private string password = "equipo1gb";
-        private string puerto = "3306";
+        private string bd = "bdescuelagbequipo1";
+        private string user = "gbEquipo1";
+        private string password = "grupo1";
+        private string port = "3306";
 
-        private string cadenaConexion => $"server={host}; database={bd}; user={usuario}; password={password}; port={puerto};";
+        //datos del servidor local 
+        private string hostLocal = "localhost";
+        private string bdLocal = "equipo1_staff_asistence";
+        private string usuarioLocal = "root";
+        private string passwordLocal = "root";
+        private string puertoLocal = "3306";
+
+        private string CadenaRemota => $"server={host}; database={bd}; user={user}; password={password}; port={port}; CharSet=utf8mb4;";
+        private string CadenaLocal => $"server={hostLocal}; database={bdLocal}; user={usuarioLocal}; password={passwordLocal}; port={puertoLocal}; CharSet=utf8mb4;";
+
         public MySqlConnection AbrirConexion()
         {
-            var conexion = new MySqlConnection(cadenaConexion);
             try
             {
+                //primero intenta conectarse al servidor remoto
+                var conexion = new MySqlConnection(CadenaRemota);
                 conexion.Open();
                 return conexion;
             }
+            catch
+            {
+                try
+                {
+                    //si el remoto falla, intenta con la base local de respaldo
+                    var conexionLocal = new MySqlConnection(CadenaLocal);
+                    conexionLocal.Open();
+                    return conexionLocal;
+                }
             catch (Exception ex)
             {
-                throw new Exception("Error al intentar conectarse a la base de datos :" + ex.Message, ex);
+                    throw new Exception("Error al intentar conectarse a la base de datos remota y local: " + ex.Message, ex);
+                }
             }
         }
+
         public void CerrarConexion(MySqlConnection conexion)
         {
             try
